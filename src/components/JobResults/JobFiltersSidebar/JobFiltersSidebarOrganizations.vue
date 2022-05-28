da65883bd48ab98c6dc61dd72f9b507605e3e18d
Rafael Nicolau <nicolaurafael@hotmail.com>
2022-05-28 19:55:40 +0000

    Create mock data slots using Accordion to filter jobs

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,37 @@
+<template>
+  <accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="w-1/2 h-8">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="Between Vue and Me" type="checkbox" class="mr-3" />
+            <label for="Between Vue and Me">Between Vue and Me</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="et vue Brute" type="checkbox" class="mr-3" />
+            <label for="et vue Brute">et vue Brute</label>
+          </li>
+          <li class="w-1/2 h-8">
+            <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
+            <label for="Vue and a Half Men">Vue and a Half Men</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </accordion>
+</template>
+
+<script>
+import Accordion from "@/components/Shared/Accordion.vue";
+
+export default {
+  name: "JobFiltersSidebarOrganizations",
+  components: {
+    Accordion,
+  },
+};
+</script>
