e2a51196e65aef011dd21632d5516047d83929fd
Rafael-Nicolau <nicolaurafael@hotmail.com>
2022-05-29 00:08:07 +0200

    Add spotlight (with slots) to hero Create tests for spotlight

---
+++ tests/unit/components/JobSearch/Spotlight.test.js
@@ -0,0 +1,60 @@
+import { mount, flushPromises } from "@vue/test-utils";
+import axios from "axios";
+jest.mock("axios");
+
+import Spotlight from "@/components/JobSearch/Spotlight.vue";
+
+describe("Spotlight", () => {
+  const mockSpotlightResponse = (data = {}) => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          img: "img",
+          title: "title",
+          description: "description",
+          ...data,
+        },
+      ],
+    });
+  };
+
+  it("provides image attribute to parent component", async () => {
+    const data = { img: "img" };
+    mockSpotlightResponse({ data });
+    const wrapper = mount(Spotlight, {
+      slots: {
+        default: `<template #default="slotProps">
+          <h1>{{ slotProps.img }}</h1>
+        </template>`,
+      },
+    });
+    await flushPromises();
+    expect(wrapper.text()).toMatch("img");
+  });
+
+  it("provides title attribute to parent component", async () => {
+    mockSpotlightResponse();
+    const wrapper = mount(Spotlight, {
+      slots: {
+        default: `<template #default="slotProps">
+          <h1>{{ slotProps.title }}</h1>
+        </template>`,
+      },
+    });
+    await flushPromises();
+    expect(wrapper.text()).toMatch("title");
+  });
+
+  it("provides image attribute to parent component", async () => {
+    mockSpotlightResponse();
+    const wrapper = mount(Spotlight, {
+      slots: {
+        default: `<template #default="slotProps">
+          <h1>{{ slotProps.description }}</h1>
+        </template>`,
+      },
+    });
+    await flushPromises();
+    expect(wrapper.text()).toMatch("description");
+  });
+});
